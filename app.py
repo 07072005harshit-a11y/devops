2dee16c6bfb6f87c131e51e1b5d1692805beccdd
07072005harshit-a11y <07072005harshit@gmail.com>
2025-08-28 14:31:40 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,4 @@
+def add(a, b):
+    return a + b
+if __name__ == "__main__":
+    print("Sum:", add(2, 3))
---
+++ test.py
@@ -0,0 +1,8 @@
+import app
+
+def test_add():
+    assert app.add(2, 3) == 5, "Sum is incorrect"
+    
+def test_add1():
+    assert app.add(10, 5) == 15
+
